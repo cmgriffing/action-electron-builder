edb57bf40a536f9f88c89d1aad65f898468c40cd
Chris Griffing <cmgriffing@gmail.com>
2022-04-27 15:52:46 -0700

    fix: handle prefix and suffix

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const runAction = async () => {
 		const resolvedPackage = resolve(pkgJsonPath)
 		const packageJsonRaw = await fs.readFile(resolvedPackage, {encoding: "utf8"});
 		const packageJson = JSON.parse(packageJsonRaw);
-		packageJson.version = customTag.slice("dev-v".length) + '-dev';
+		packageJson.version = customTag.slice(1) + '-dev';
 		await fs.writeFile(resolvedPackage, JSON.stringify(packageJson, null, 2));
 	}
 
